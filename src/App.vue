f852ddd895c5f8150e5dccc45eab1477748714ca
soo970220-ship-it <soo970220@gmail.com>
2026-04-17 16:33:41 +0900

    260417[이론]이벤트~감시자

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
 import DataBinding from './components/DataBinding.vue';
-
+import EventHandling from './components/EventHandling.vue';
+import ConditionalRendering from './components/ConditionalRendering.vue';
+import ListRandering from './components/ListRandering.vue';
+import Watchers from './components/Watchers.vue';
 // js코드작성부분
 
 </script>
 
 <template>
-  <!-- HTML 작성부분 -->
-   <DataBinding />
+<!-- HTML 작성부분 -->
+<!-- 조건부 랜더링 -->
+   <DataBinding v-if="false"/>  
+   <EventHandling v-if="false"/>
+   <ConditionalRendering v-if="false"/>
+   <ListRandering v-if="false"/>
+   <Watchers/>
+
 </template>
 
 <style>
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref} from 'vue';
+
+const cnt = ref(0);
+let test=ref(0);
+
+test.value=1;
+
+function resetCnt(){
+    cnt.value = 0;
+ }
+</script>
+
+<template>
+<h1>이벤트 핸들링: {{ cnt }}</h1>
+<button type="button" @mousemove="cnt++">인라인 핸들러</button>
+<button type="button" @click="resetCnt(9)">메소드 핸들러</button>
+<input type="text" @keydown="cnt++" v-model="cnt">
+</template>
+
+<style>
+
+</style>
